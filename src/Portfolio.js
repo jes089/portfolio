44fd63e24af4705cc7005bf4ceeccf2285d704cc
jes089 <j.yuan.sun@gmail.com>
2023-07-22 16:10:34 -0700

    move to portfolio

--- src/Portfolio.js
+++ src/Portfolio.js
@@ -1,9 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function Portfolio() {
   return (
-    <div className="App">
+    <div className="Portfolio">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -22,4 +22,4 @@ function App() {
   );
 }
 
-export default App;
+export default Portfolio;
