b390d9e79f8a84e9723021edfeca953e2ada5867
jes089 <j.yuan.sun@gmail.com>
2023-07-29 15:44:40 -0700

    add title and image

--- src/Portfolio.js
+++ src/Portfolio.js
@@ -1,12 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import jenImage from './images/jen.png'
+import { Container, Grid,Typography } from '@mui/material';
 
 function Portfolio() {
   return (
-    <div className="Portfolio">
-      <header className="App-header">
-      </header>
-    </div>
+    <Container className="portfolio">
+      <div style={{display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'center'}}>
+        <div style={{paddingRight: '1rem'}}>
+         <img className="headshot" width="150" src={jenImage} alt="image of Jen" /> 
+        </div>
+        <div>
+        <Typography variant='h3' >Jennifer Sun</Typography>
+          <Typography variant='h4'>UX/UI Designer && Frontend Developer</Typography>
+          {/* <Typography variant='h3'>Frontend Developer </Typography> */}
+        </div>
+      </div>
+    </Container>
   );
 }
 
